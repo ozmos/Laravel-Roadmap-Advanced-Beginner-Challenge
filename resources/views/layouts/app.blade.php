8a275d20877040c60f3ad832949eb05f1abc34bb
Osamu Morozumi <39433568+ozmos@users.noreply.github.com>
2022-05-02 09:25:02 +0800

    refactor demo templates, import markdown reader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,61 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard - Admin One Tailwind CSS Admin Dashboard</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Tailwind is included -->
+    <link rel="stylesheet" href="/css/tailwind-admin.css?v=1628755089081">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#00b4b6" />
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <meta name="description" content="Admin One - free Tailwind dashboard">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+    <meta property="og:url" content="https://justboil.github.io/admin-one-tailwind/">
+    <meta property="og:site_name" content="JustBoil.me">
+    <meta property="og:title" content="Admin One HTML">
+    <meta property="og:description" content="Admin One - free Tailwind dashboard">
+    <meta property="og:image" content="https://justboil.me/images/one-tailwind/repository-preview-hi-res.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1920">
+    <meta property="og:image:height" content="960">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:title" content="Admin One HTML">
+    <meta property="twitter:description" content="Admin One - free Tailwind dashboard">
+    <meta property="twitter:image:src" content="https://justboil.me/images/one-tailwind/repository-preview-hi-res.png">
+    <meta property="twitter:image:width" content="1920">
+    <meta property="twitter:image:height" content="960">
+
+</head>
+
+<body class="font-sans antialiased">
+    <div id="app">
+        <x-navigation></x-navigation>
+        <x-sidebar></x-sidebar>
+        <x-title-bar></x-title-bar>
+        <section class="section main-section">
+        {{ $slot }}
+        <x-pagination></x-pagination>
+        </section>
+        <x-footer></x-footer>
+    </div>
+
+<!-- Scripts below are for demo only -->
+<script type="text/javascript" src="/js/main.min.js?v=1628755089081"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+<script type="text/javascript" src="/js/chart.sample.min.js"></script>
+
+
+<!-- Icons below are for demo only. Feel free to use any icon pack. Docs: https://bulma.io/documentation/elements/icon/ -->
+<link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css">
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('readme');
 });
 
 Route::group(['prefix' => 'demo'], function() {
